acd76924ca18f479223309a700c1f09635c3a454
DiegoNatanael <gonzalezdiegonatanael@gmail.com>
2026-04-19 20:40:55 -0600

    Arreglo de errores globales

--- app/Models/Generation.php
+++ app/Models/Generation.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable(['user_id', 'type', 'topic'])]
 class Generation extends Model
 {
-
+    /**
+     * A generation belongs to a user (inverse one-to-many).
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
--- config/sanctum.php
+++ config/sanctum.php
@@ -1,2 +1,81 @@
+<?php
 
-php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"
+use Laravel\Sanctum\Sanctum;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stateful Domains
+    |--------------------------------------------------------------------------
+    |
+    | Requests from the following domains / hosts will receive stateful API
+    | authentication cookies. Typically, these should include your local
+    | and production domains which access your API via a frontend.
+    |
+    */
+
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
+    ))),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Guards
+    |--------------------------------------------------------------------------
+    |
+    | This array contains the authentication guards that will be checked when
+    | Sanctum is trying to authenticate a request. If none of these guards
+    | are able to authenticate the request, Sanctum will use the default
+    | guard configurations.
+    |
+    */
+
+    'guard' => ['web'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Expiration Minutes
+    |--------------------------------------------------------------------------
+    |
+    | This value controls the number of minutes until an issued token will be
+    | considered expired. If this value is null, personal access tokens have
+    | no expiration time.
+    |
+    */
+
+    'expiration' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Token Prefix
+    |--------------------------------------------------------------------------
+    |
+    | Sanctum can prefix new tokens in order to take advantage of various
+    | automated security scanning tools. See the documentation for more
+    | information.
+    |
+    */
+
+    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Middleware
+    |--------------------------------------------------------------------------
+    |
+    | When authenticating your multi-domain or single-page application, Sanctum
+    | will need to inject some middleware into Laravel's global middleware
+    | stack in order to properly handle CORS and CSRF options.
+    |
+    */
+
+    'middleware' => [
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => App\Http\Middleware\ValidateCsrfToken::class,
+    ],
+
+];
